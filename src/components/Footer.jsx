050172a9edd390f09e781d453ac2d8e93bc440b2
CHAREN Bahaeddine hemmem <charenbahaeddinehemmem@gmail.com>
2024-12-06 21:37:02 +0100

    Footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,7 @@
-export const Footer = () => {};
+export const Footer = () => {
+  return (
+    <div className="max-w-3xl mx-auto flex justify-between items-center px-4">
+      <p className="tracking-tight m-4 p-4">Copyright © 2024 Hemmem CH.</p>
+    </div>
+  );
+};
