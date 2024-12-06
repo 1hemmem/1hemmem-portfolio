45f42bb7cea153442d9d889361a0207e29d003bb
CHAREN Bahaeddine hemmem <charenbahaeddinehemmem@gmail.com>
2024-12-06 22:49:56 +0100

    removed comments

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -20,20 +20,6 @@ export const Certifications = () => {
           ))}
         </div>
       </section>
-
-      {/* <section id="Courses" className="mt-5">
-      <h1 className="text-xl font-bold">Courses</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-1">
-        {Courses.map((val,index)=>(
-          <CertificateCard
-          key={val.id || index}
-          Title={val.Title}
-          Url={val.Url}
-          From={val.From}
-          />
-        ))}
-        </div>
-      </section> */}
     </div>
   );
 };
