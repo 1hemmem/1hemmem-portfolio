e94a17221bc0b7e0645dfbae66ad988fa1a73678
1hemmem <charenbahaeddinehemmem@gmail.com>
2025-03-08 02:33:08 +0100

    style: better style for the navbar

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -68,11 +68,11 @@ export function Tools() {
         alt="git"
         className="h-[50px] w-auto"
       />
-      <img
+      {/* <img
         src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
         alt="Mysql"
         className="h-[50px] w-auto"
-      />
+      /> */}
       <img
         src="https://camo.githubusercontent.com/8b690f4dff81513c7425f3b8f6e66b34a1dea43e22562037eeb5449d18571c89/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6d7973716c2f6d7973716c2d6f726967696e616c2e737667"
         alt="Postgresql"
