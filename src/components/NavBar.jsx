e6835f73c3f2875acee4998fecc8e5c469625390
1hemmem <charenbahaeddinehemmem@gmail.com>
2025-03-09 00:53:01 +0100

    comm

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -38,7 +38,15 @@ export default function NavBar() {
           }`}
         >
           Projects
-        </a>{" "}
+        </a>
+        <a
+          href="/blogs"
+          className={`hover:text-gray-400 hover:underline transition-colors duration-500 ${
+            isActive("/blogs") ? "font-bold text-white underline" : ""
+          }`}
+        >
+          Blogs
+        </a>
       </div>
     </nav>
   );
