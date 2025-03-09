839013e153bf04cbfb5e5c2d75733d0accee7d54
1hemmem <charenbahaeddinehemmem@gmail.com>
2025-03-09 02:30:15 +0100

    style: changed the ui of connect component

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -1,4 +1,4 @@
-import { Github, Linkedin, Mail, Twitter, ArrowRight } from "lucide-react";
+import { Github, Linkedin, Mail, Twitter } from "lucide-react";
 
 export default function Connect() {
   const socialLinks = [
@@ -30,31 +30,40 @@ export default function Connect() {
 
   return (
     <div className="py-20">
-      <h1 className="text-3xl font-bold mb-2">Find me here</h1>
-      <p className="mb-6">Reach out to me on social media or via email</p>
+      <h1 className="text-3xl font-bold text-black dark:text-white mb-2">Find me here</h1>
+      <p className="text-gray-800 dark:text-gray-300 mb-6">Reach out to me on social media or via email</p>
 
-      <div className="space-y-3">
+      <div className="space-y-4">
         {socialLinks.map((link) => (
-          <a
-            key={link.platform}
-            href={link.url}
-            target={link.platform !== "Email" ? "_blank" : undefined}
-            rel={link.platform !== "Email" ? "noopener noreferrer" : undefined}
-            className="flex items-center justify-between p-3 rounded-lg transition-all hover:bg-zinc-800 group border border-transparent"
-          >
-            <div className="flex items-start gap-3">
-              {link.icon}
-              <span className="font-semibold">{link.platform}:</span>
-              <span className="text-muted-foreground font-medium">
-                {link.username}
+          <div key={link.platform}>
+            <a
+              href={link.url}
+              target={link.platform !== "Email" ? "_blank" : undefined}
+              rel={link.platform !== "Email" ? "noopener noreferrer" : undefined}
+              className="not-prose group hover:text-muted-foreground inline-flex items-center font-mono font-semibold"
+            >
+              <span className="flex items-center gap-2">
+                {link.icon}
+                <span className="font-semibold">{link.username}</span>
               </span>
-            </div>
-
-            <ArrowRight
-              className="h-4 w-4 text-muted-foreground transform transition-all duration-300 ease-in-out 
-            group-hover:translate-x-2 group-hover:scale-110 group-hover:text-primary"
-            />
-          </a>
+              <svg
+                viewBox="0 0 10 30"
+                className="size-4 fill-none stroke-current stroke-[2px] transition-opacity duration-300 ease-in-out ml-1 "
+              >
+                <line
+                  x1="5"
+                  y1="12"
+                  x2="19"
+                  y2="12"
+                  className="translate-x-[10px] scale-x-0 transition-transform duration-300 ease-in-out group-hover:translate-x-0 group-hover:scale-x-100"
+                ></line>
+                <polyline
+                  points="12 5 19 12 12 19"
+                  className="-translate-x-2 transition-transform duration-300 ease-in-out group-hover:translate-x-0"
+                ></polyline>
+              </svg>
+            </a>
+          </div>
         ))}
       </div>
     </div>
